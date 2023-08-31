0d0d98074f2de320f9035267ecc38ffb10124d44
Vignesh Esakkiappan <85873362+Papavig@users.noreply.github.com>
2023-08-31 10:15:29 +0530

    first commit

---
+++ src/budgetbuddy/BudgetBuddy.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package budgetbuddy;
+
+public class BudgetBuddy {
+
+    public static void main(String[] args) {
+       
+     java.awt.EventQueue.invokeLater(() -> {
+         new homeGUI().setVisible(true);
+     });
+    }
+}
---
+++ src/budgetbuddy/dbConnect.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package budgetbuddy;
+
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author being
+ */
+public class dbConnect {
+    public static Connection c;
+    public static Statement state;
+    
+    static{
+        try{
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/BudgetBuddydb?useSSL=false", "root", "NOOBisPASS");
+            state = c.createStatement();
+        }catch (HeadlessException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+}
